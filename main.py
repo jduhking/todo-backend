4b82c6e80ea193437f756ca0affe2296625b7303
jduhking <jamesbodebiyi@gmail.com>
2024-03-24 14:50:31 -0500

    completed todo app backend

---
+++ main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+import json
+from uuid import uuid4
+
+origins = [
+    "http://localhost:5173"
+]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+"""
+Todos will have the shape of 
+{
+    id: a unique id, use the uuid4 package
+    message: what to do
+    complete: whether the task has been completed or not
+}
+"""
+todos = []
+
+@app.get("/")
+def read_root():
+    return "Hello World"
+
+@app.get("/todos")
+def get_todos(): 
+    return todos
+
+@app.get("/todos/{id}")
+def get_todo(id: str):
+    """ loop through all the todos and return the one which matches the id """
+    for todo in todos:
+        if todo.id == id:
+            return todo
+    return None
+
+@app.post("/todo")
+async def create_todo(request: Request):
+    """ create a todo and append it to the end of the todos list """
+    todo = json.loads(await request.body())
+    new_todo = {
+        "id" : str(uuid4()),
+        "message" : todo["message"],
+        "completed" : False
+    }
+    todos.append(new_todo)
+    return todo
+
+@app.post("/todo/toggle/{id}")
+async def toggle_todo_completion(id: str):
+    """ if the todo hasnt been marked as complete then check it else uncheck it """
+    # search for the todo in the list with the matching id
+    for todo in todos:
+        if todo["id"] == id:
+            # toggle its complete status
+            todo["completed"] = True if todo["completed"] == False else False
+            return True
+
+    return False # could not find the task with the given id
